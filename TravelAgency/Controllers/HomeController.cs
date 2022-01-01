3925b1030c83b80d79934ac408500158d3d8f359
maciej.fuhrmann <maciej.fuhrmann@happyteam.io>
2022-01-01 17:21:30 +0100

    Created dropdown for dates in form. Added new model to display correct values in view.

--- TravelAgency/Controllers/HomeController.cs
+++ TravelAgency/Controllers/HomeController.cs
@@ -1,26 +1,52 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using TravelAgency.Models;
+using TravelAgencyAPI.Models;
+
 
 namespace TravelAgency.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly HttpClient client;
         private readonly ILogger<HomeController> _logger;
+        private readonly string WebApiPath;
+        private readonly IConfiguration _configuration;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
         {
+            _configuration = configuration;
             _logger = logger;
+            WebApiPath = _configuration["TravelAgencyConfig:Url"];
+            client = new HttpClient();
+            client.DefaultRequestHeaders.Add("ApiKey", _configuration["TravelAgencyConfig:ApiKey"]);
         }
 
-        public IActionResult Index()
+        public async Task<ActionResult> Index()
         {
-            return View();
+            List<Offer> offers = new List<Offer>();
+            HttpResponseMessage response = await client.GetAsync(WebApiPath);
+            if (response.IsSuccessStatusCode)
+            {
+                offers = await response.Content.ReadAsAsync<List<Offer>>();
+            }
+            var formViewModel = new FormViewModel
+            {
+                OfferList = offers.Select(x => new SelectListItem
+                {
+                    Value = x.ID.ToString(),
+                    Text = x.FromDate.Date.ToShortDateString()
+                })
+            };
+            return View(formViewModel);
         }
 
         public IActionResult Privacy()
---
+++ TravelAgency/Models/FormViewModel.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TravelAgencyAPI.Models;
+
+namespace TravelAgency.Models
+{
+    public class FormViewModel
+    {
+        public IEnumerable<SelectListItem> OfferList { get; set; }
+        public string SelectedDate { get; set; }
+        public Offer Offer { get; set; }
+        public City City { get; set; }
+        public Country Country { get; set; }
+    }
+}
